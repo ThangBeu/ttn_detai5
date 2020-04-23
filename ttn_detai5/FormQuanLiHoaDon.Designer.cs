906e6fe27bb4f52f4da3f28866b56e4c564bb696
VanHieu <vanhieutb1999@gmail.com>
2020-04-24 03:13:58 +0700

    form QuanLyHoaDOn

--- ttn_detai5/FormQuanLiHoaDon.Designer.cs
+++ ttn_detai5/FormQuanLiHoaDon.Designer.cs
@@ -28,123 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewHD = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
-            this.btnSearchKH = new System.Windows.Forms.Button();
-            this.txtSearchKH = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearchHD = new System.Windows.Forms.Button();
+            this.txtSearchHD = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHD)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewHD
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 69);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(959, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridViewHD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHD.Location = new System.Drawing.Point(10, 56);
+            this.dataGridViewHD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewHD.Name = "dataGridViewHD";
+            this.dataGridViewHD.RowHeadersWidth = 51;
+            this.dataGridViewHD.RowTemplate.Height = 24;
+            this.dataGridViewHD.Size = new System.Drawing.Size(719, 122);
+            this.dataGridViewHD.TabIndex = 0;
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(12, 246);
+            this.dataGridView2.Location = new System.Drawing.Point(9, 200);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(959, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(719, 122);
             this.dataGridView2.TabIndex = 1;
             // 
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(12, 423);
+            this.dataGridView3.Location = new System.Drawing.Point(9, 344);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(959, 150);
+            this.dataGridView3.Size = new System.Drawing.Size(719, 122);
             this.dataGridView3.TabIndex = 2;
             // 
-            // btnSearchKH
+            // btnSearchHD
             // 
-            this.btnSearchKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnSearchKH.Location = new System.Drawing.Point(358, 25);
-            this.btnSearchKH.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSearchKH.Name = "btnSearchKH";
-            this.btnSearchKH.Size = new System.Drawing.Size(176, 37);
-            this.btnSearchKH.TabIndex = 78;
-            this.btnSearchKH.Text = "Search";
-            this.btnSearchKH.UseVisualStyleBackColor = true;
+            this.btnSearchHD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnSearchHD.Location = new System.Drawing.Point(268, 20);
+            this.btnSearchHD.Name = "btnSearchHD";
+            this.btnSearchHD.Size = new System.Drawing.Size(132, 30);
+            this.btnSearchHD.TabIndex = 78;
+            this.btnSearchHD.Text = "Search";
+            this.btnSearchHD.UseVisualStyleBackColor = true;
+            this.btnSearchHD.Click += new System.EventHandler(this.btnSearchHD_Click);
             // 
-            // txtSearchKH
+            // txtSearchHD
             // 
-            this.txtSearchKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtSearchKH.Location = new System.Drawing.Point(58, 30);
-            this.txtSearchKH.Margin = new System.Windows.Forms.Padding(4);
-            this.txtSearchKH.Name = "txtSearchKH";
-            this.txtSearchKH.Size = new System.Drawing.Size(292, 27);
-            this.txtSearchKH.TabIndex = 77;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(264, 123);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 17);
-            this.label1.TabIndex = 79;
-            this.label1.Text = "Dữ Liệu Hóa Đơn";
+            this.txtSearchHD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtSearchHD.Location = new System.Drawing.Point(44, 24);
+            this.txtSearchHD.Name = "txtSearchHD";
+            this.txtSearchHD.Size = new System.Drawing.Size(220, 23);
+            this.txtSearchHD.TabIndex = 77;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(264, 278);
+            this.label2.Location = new System.Drawing.Point(198, 226);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.Size = new System.Drawing.Size(105, 13);
             this.label2.TabIndex = 80;
             this.label2.Text = "Bảng 1 bên hóa đơn";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(264, 478);
+            this.label3.Location = new System.Drawing.Point(198, 388);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(136, 17);
+            this.label3.Size = new System.Drawing.Size(104, 13);
             this.label3.TabIndex = 81;
             this.label3.Text = "bảng 2 bên hóa đơn";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(-19, -19);
+            this.button1.Location = new System.Drawing.Point(-14, -15);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 82;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(-19, -19);
+            this.button2.Location = new System.Drawing.Point(-14, -15);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(56, 19);
             this.button2.TabIndex = 83;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(-19, -19);
+            this.button3.Location = new System.Drawing.Point(-14, -15);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(56, 19);
             this.button3.TabIndex = 84;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -152,34 +149,36 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(782, 25);
+            this.button4.Location = new System.Drawing.Point(559, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(137, 32);
+            this.button4.Size = new System.Drawing.Size(130, 35);
             this.button4.TabIndex = 85;
             this.button4.Text = "Xóa Hóa Đơn";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // FormQuanLiHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 585);
+            this.ClientSize = new System.Drawing.Size(738, 475);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnSearchKH);
-            this.Controls.Add(this.txtSearchKH);
+            this.Controls.Add(this.btnSearchHD);
+            this.Controls.Add(this.txtSearchHD);
             this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewHD);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormQuanLiHoaDon";
             this.Text = "FormQuanLiHoaDon";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.FormQuanLiHoaDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHD)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
@@ -189,12 +188,11 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewHD;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView3;
-        private System.Windows.Forms.Button btnSearchKH;
-        private System.Windows.Forms.TextBox txtSearchKH;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearchHD;
+        private System.Windows.Forms.TextBox txtSearchHD;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
--- ttn_detai5/FormQuanLiHoaDon.cs
+++ ttn_detai5/FormQuanLiHoaDon.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ttn_detai5;
 
 namespace ttn_detai5
 {
     public partial class FormQuanLiHoaDon : Form
     {
+        private static SqlConnection conn = new SqlConnection(DBAccess.strConn);
+        private static SqlDataAdapter adt = new SqlDataAdapter();
+        private static SqlCommand cmd = new SqlCommand();
+        DBAccess access = new DBAccess();
+        DataTable table, table2, table3;
+
+        private void FormQuanLiHoaDon_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            GetData("select * from HOADON", dataGridViewHD, table);
+        }
+
+        private void btnSearchHD_Click(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            string key = txtSearchHD.Text.Trim();
+            if (key.Equals(""))
+            {
+                string query = "SELECT * FROM dbo.HOADON";
+                GetData(query, dataGridViewHD, table);
+            }
+            else
+            {
+                string query = "SELECT * FROM dbo.HOADON WHERE MaHD = '" + key + "' OR MaNV = '" + key + "' OR MaKH = '" + key + "' ";
+                GetData(query, dataGridViewHD, table);
+            }
+        }
+
         public FormQuanLiHoaDon()
         {
             InitializeComponent();
         }
+        private void GetData(string query, DataGridView grid, DataTable table)
+        {
+            access.createConn();
+            adt = new SqlDataAdapter(query, conn);
+            adt.Fill(table);
+            grid.DataSource = table;
+            conn.Close();
+        }
+
     }
 }
