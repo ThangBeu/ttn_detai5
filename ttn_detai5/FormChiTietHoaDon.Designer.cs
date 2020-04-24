c90a589aa1304097ba5166ec154c015b5c422504
VanHieu <vanhieutb1999@gmail.com>
2020-04-24 14:55:29 +0700

    add form ChiTietHoaDon

---
+++ ttn_detai5/FormChiTietHoaDon.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ttn_detai5
+{
+    partial class FormChiTietHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMaHD = new System.Windows.Forms.Label();
+            this.dataGridViewPhong = new System.Windows.Forms.DataGridView();
+            this.dataGridViewDichVu = new System.Windows.Forms.DataGridView();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPhong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDichVu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(289, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã hóa đơn : ";
+            // 
+            // lblMaHD
+            // 
+            this.lblMaHD.AutoSize = true;
+            this.lblMaHD.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaHD.Location = new System.Drawing.Point(442, 34);
+            this.lblMaHD.Name = "lblMaHD";
+            this.lblMaHD.Size = new System.Drawing.Size(35, 24);
+            this.lblMaHD.TabIndex = 1;
+            this.lblMaHD.Text = ". . .";
+            // 
+            // dataGridViewPhong
+            // 
+            this.dataGridViewPhong.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridViewPhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPhong.Location = new System.Drawing.Point(28, 128);
+            this.dataGridViewPhong.Name = "dataGridViewPhong";
+            this.dataGridViewPhong.Size = new System.Drawing.Size(345, 213);
+            this.dataGridViewPhong.TabIndex = 2;
+            // 
+            // dataGridViewDichVu
+            // 
+            this.dataGridViewDichVu.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridViewDichVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDichVu.Location = new System.Drawing.Point(427, 128);
+            this.dataGridViewDichVu.Name = "dataGridViewDichVu";
+            this.dataGridViewDichVu.Size = new System.Drawing.Size(344, 213);
+            this.dataGridViewDichVu.TabIndex = 3;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(328, 390);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(130, 35);
+            this.btnThoat.TabIndex = 86;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // FormChiTietHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.dataGridViewDichVu);
+            this.Controls.Add(this.dataGridViewPhong);
+            this.Controls.Add(this.lblMaHD);
+            this.Controls.Add(this.label1);
+            this.Name = "FormChiTietHoaDon";
+            this.Text = "FormChiTietHoaDon";
+            this.Load += new System.EventHandler(this.FormChiTietHoaDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPhong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDichVu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMaHD;
+        private System.Windows.Forms.DataGridView dataGridViewPhong;
+        private System.Windows.Forms.DataGridView dataGridViewDichVu;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
---
+++ ttn_detai5/FormChiTietHoaDon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ttn_detai5
+{
+    public partial class FormChiTietHoaDon : Form
+    {
+        private static SqlConnection conn = new SqlConnection(DBAccess.strConn);
+        private static SqlDataAdapter adt = new SqlDataAdapter();
+        private static SqlCommand cmd = new SqlCommand();
+        DBAccess access = new DBAccess();
+        DataTable table,table2;
+
+        string mahd = FormQuanLiHoaDon.mahd;
+        public FormChiTietHoaDon()
+        {
+            InitializeComponent();
+        }
+        private void GetData(string query, DataGridView grid, DataTable table)
+        {
+            access.createConn();
+            adt = new SqlDataAdapter(query, conn);
+            adt.Fill(table);
+            grid.DataSource = table;
+            conn.Close();
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FormChiTietHoaDon_Load(object sender, EventArgs e)
+        {
+            lblMaHD.Text = mahd;
+            table = new DataTable();
+            table2 = new DataTable();
+            string queryChiTietPhong = "SELECT * FROM dbo.HOADON_PHONG " +
+                " WHERE MaHD = '" + mahd + "'";
+            GetData(queryChiTietPhong, dataGridViewPhong, table);
+
+            string queryChiTietDichVu = "SELECT * FROM dbo.HOADON_DICHVU " +
+                " WHERE MaHD = '" + mahd + "'";
+            GetData(queryChiTietDichVu, dataGridViewDichVu, table2);
+
+        }
+    }
+}
